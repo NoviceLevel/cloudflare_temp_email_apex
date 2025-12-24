2756efc58ac95fa76573d7fd4716aa87f290f9f1
NoviceLevel <62823553+NoviceLevel@users.noreply.github.com>
2025-12-24 23:30:06 +0800

    feat: update

---
+++ vite.config.js
@@ -0,0 +1,67 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import vuetify from 'vite-plugin-vuetify'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: './dist',
+    target: 'es2015',
+  },
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    wasm(),
+    topLevelAwait(),
+    legacy({
+      targets: ['iOS >= 12', 'Chrome >= 70', 'Safari >= 12', 'Firefox >= 60'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      modernPolyfills: true,
+    }),
+    VitePWA({
+      registerType: null,
+      devOptions: {
+        enabled: false
+      },
+      workbox: {
+        disableDevLogs: true,
+        globPatterns: [],
+        runtimeCaching: [],
+        navigateFallback: null,
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        name: 'Temp Email',
+        short_name: 'Temp Email',
+        description: 'Temp Email - Temporary Email',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  }
+})
