8ff9f2dfc3908cea357d7095c7c67fa972222be5
NoviceLevel <62823553+NoviceLevel@users.noreply.github.com>
2025-12-24 14:43:44 +0800

    chore: Remove community links from About page and README

--- frontend/src/views/common/About.vue
+++ frontend/src/views/common/About.vue
@@ -8,18 +8,7 @@ const { announcement } = useGlobalState()
         <v-card variant="flat" max-width="800">
             <v-card-text>
                 <div v-html="announcement"></div>
-                <div class="d-flex ga-2 flex-wrap mt-4">
-                    <v-btn href="https://github.com/dreamhunter2333/cloudflare_temp_email" target="_blank"
-                        prepend-icon="mdi-github">
-                        Github
-                    </v-btn>
-                    <v-btn href="https://discord.gg/dQEwTWhA6Q" target="_blank" prepend-icon="mdi-discord">
-                        Discord
-                    </v-btn>
-                    <v-btn href="https://t.me/cloudflare_temp_email" target="_blank" prepend-icon="mdi-send">
-                        Telegram
-                    </v-btn>
-                </div>
+
             </v-card-text>
         </v-card>
     </div>
