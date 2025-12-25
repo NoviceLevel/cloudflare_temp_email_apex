8c1d03a8356109113ec23c55b4a074e403a4c31c
NoviceLevel <62823553+NoviceLevel@users.noreply.github.com>
2025-12-25 15:33:35 +0800

    Add M3 loading indicator to About page

--- frontend/src/views/common/About.vue
+++ frontend/src/views/common/About.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useGlobalState } from '../../store'
+import M3LoadingIndicator from '../../components/M3LoadingIndicator.vue'
 const { announcement } = useGlobalState()
 </script>
 
@@ -8,7 +9,8 @@ const { announcement } = useGlobalState()
         <v-card variant="flat" max-width="800" width="100%">
             <v-card-text>
                 <div v-html="announcement"></div>
-                <div class="d-flex align-center justify-center mt-6">
+                <div class="d-flex flex-column align-center justify-center mt-6 ga-4">
+                    <M3LoadingIndicator />
                     <v-chip variant="outlined" color="primary" size="small">
                         M3 Expressive
                     </v-chip>
